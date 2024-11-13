fb9672b9da6e5f729a4e617bab466a7633ff00ee
Korkin Alexsandr <korkin.06@inbox.ru>
2024-11-13 14:52:11 +0300

    Add Hash class

---
+++ Tasks/WpfApp1/Pract_3/Hash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_3
+{
+    internal class Hash
+    {
+        public static string createHash(string stringInput)
+        {
+            SHA256 hash = SHA256.Create();
+
+            return BitConverter.ToString(hash.ComputeHash(Encoding.UTF8.GetBytes(stringInput)));
+        }
+    }
+}
